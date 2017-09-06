6dcc222b02d8ab682b4e6f6fc4614ddfb5413e1c
sbarnwell88 <susy6688@gmail.com>
2017-09-06 17:02:16 -0400

    added controllers

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -10,11 +10,29 @@ class CreaturesController < ApplicationController
     def create 
         @creature = Creature.create!(creature_params)
 
-        redirect_to creatures_path
+        redirect_to creature_path(@creature)
     end
 
     def show
         @creature = Creature.find params[:id]
+    end
+
+    def edit
+        @creature = Creature.find params[:id]
+    end
+
+    def update
+        @creature = Creature.find params[:id]
+        @creature.update(creature_params)
+
+        redirect_to creature_path(@creature)
+    end
+
+    def destroy
+        @creature = Creature.find params[:id]
+        @creature.destroy
+
+        redirect_to creatures_path
     end
 
     private
